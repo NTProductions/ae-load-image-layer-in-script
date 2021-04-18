ae4bbcfaca97f6355b9e52da119ab4ea167d2fdc
Nate Lovell <Natelovell99@gmail.com>
2021-04-18 03:46:48 -0600

    Create Load Image Layer in Script.jsx

---
+++ Load Image Layer in Script.jsx
@@ -0,0 +1,10 @@
+﻿var pngLayer = app.project.activeItem.layer(1); // put your reference to the png layer here
+var pngItem = pngLayer.source;
+var pngFile = pngItem.file;
+
+// now use the pngFile to populate an image in your script (here's just a super basic script to have an image in it)
+var window = new Window("palette", "imageLayerLoader", undefined);
+var image = window.add("image", undefined, pngFile);
+
+window.center();
+window.show();
